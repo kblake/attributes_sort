8495e1a5f9c51850e84c1a75e117db7bf1119e06
Karmen Blake <karmenblake@gmail.com>
2009-10-30 17:01:59 -0700

    Initial commit and copy of work started elsewhere. still need to add more tests

---
+++ lib/attributes_sort.rb
@@ -0,0 +1,21 @@
+module AttributesSort
+  def self.included(receiver)
+    Array.class_eval do
+      def do_sort(options = {})
+				raise "You must pass in sort_by criteria" unless options[:sort_by]
+        type = self.first.class
+        type.instance_eval do
+          def do_attributes_sort(collection, options={})
+            attributes = options[:sort_by].inject("["){|attribute_string, attribute| attribute_string << "object.#{attribute},"} + "]"
+            collection.sort_by{|object| eval(attributes)}
+          end
+        end
+				begin
+        	type.do_attributes_sort(self, options)
+				rescue NoMethodError
+					raise "You must sort by an attribute that exists in the object that you are sorting"
+				end
+      end
+    end
+  end
+end
--- spec/attributes_sort_spec.rb
+++ spec/attributes_sort_spec.rb
@@ -1,7 +1,57 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "AttributesSort" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+class Person
+  include AttributesSort
+
+  attr_accessor :firstname,:lastname,:age
+  
+  def initialize(firstname="",lastname="",age=0)
+    @firstname = firstname
+    @lastname = lastname
+    @age = age
   end
+end
+
+
+describe "AttributesSort" do
+	before(:each) do
+		@p1 = Person.new("joe","blow",89)
+		@p2 = Person.new("joe","blow",12)
+		@p3 = Person.new("mary","watson",32)
+		@p4 = Person.new("annie","watson",9)
+		@p5 = Person.new("bob","builder",12)
+		@people = [@p1, @p2, @p3, @p4, @p5]			
+	end
+
+	def test_sort(criteria, result)
+		@people.do_sort(:sort_by => criteria).should == result		
+	end
+
+  it "object must have attr accessible attributes for sort_by to work" do
+  	p = Person.new
+		p.should respond_to(:firstname)
+		p.should respond_to(:lastname)
+		p.should respond_to(:age)
+	end
+
+	it "sort by last name" do
+		test_sort([:lastname], [@p1, @p2, @p5, @p4, @p3])
+	end
+
+	it "sort by age" do
+		test_sort([:age], [@p4, @p2, @p5, @p3, @p1])
+	end
+
+	it "sort by last name then by first name then by age" do
+		test_sort([:lastname,:firstname,:age], [@p2, @p1, @p5, @p4, @p3])
+	end
+
+	it "raise error if criteria is not provided" do
+		lambda {test_sort(nil, [@p1, @p2, @p5, @p4, @p3])}.should raise_error("You must pass in sort_by criteria")
+	end
+
+	it "raise error if criteria is not a valid attribute of class" do
+		lambda {test_sort([:blah], [@p1, @p2, @p5, @p4, @p3])}.should raise_error("You must sort by an attribute that exists in the object that you are sorting")		
+	end
+	
 end
